b05ddcf056df06acc2baf9a479c7391c34f22954
SaqibAhmadBhat <154875948+SaqibAhmadBhat@users.noreply.github.com>
2024-02-20 23:03:18 +0530

    Update 03-factorial of a number using recursion.py
    changes demanded by aqib majeed

--- 03-factorial of a number using recursion.py
+++ 03-factorial of a number using recursion.py
@@ -1,12 +1,26 @@
-# factorial of a number using recursion
+while True:
+    try:
+        n = int(input("Enter a positive integer: "))
+        if n <= 0:
+            raise ValueError("Please enter a positive integer.")
+        
+        def fact(n):
+            if n == 1:
+                return 1
+            else:
+                return n * fact(n - 1)
 
-def fact(n):
-    if n == 1:
-        return 1
-    else:
-        return n*fact(n-1)
+        res = fact(n)
+        print("The factorial of the given number:", res)
 
+    except ValueError as ve:
+        print(f"Error: {ve}")
+        continue
 
-n = int(input("Enter positive integer: "))
-res = fact(n)
-print("The factorial of a given number: ", res)
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        continue
+
+    ch = input("Do you want to continue? (y/n) ")
+    if ch.lower() != 'y':
+        break
